09a1e4642e242d16730049f67beeb43bac11ac3e
Rumesh Chathuranga <rumarumesh1220@gmail.com>
2025-03-14 21:08:30 +0530

    Create heap

---
+++ Heap/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+  
+// function to heapify the tree
+void heapify(int arr[], int n, int root)
+{
+   // build heapify
+   
+    int largest = root; // Initialize largest as root
+    int left = 2 * root + 1; // left = 2*root + 1
+    int right = 2 * root + 2; // right = 2*root + 2
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != root) {
+        swap(arr[root], arr[largest]); // Swap root with largest
+
+        // Recursive call
+        heapify(arr, n, largest);
+    }
+   
+}
+  
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+   // build heap
+   
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // extracting elements from heap one by one
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]); // Send current root to end
+
+        // call max heapify
+        heapify(arr, i, 0);
+    }
+  
+   
+}
+  
+/* print contents of array */
+void displayArray(int arr[], int n)
+{
+   for (int i=0; i<n; ++i)
+   cout << arr[i] << " ";
+   cout << "\n";
+}
+  
+// main program
+int main()
+{
+    string input;
+    getline(cin, input);
+
+    stringstream ss(input);
+    int num;
+    vector<int> heap_arr;
+
+    while (ss >> num) {
+        heap_arr.push_back(num);
+    }
+
+    int n = heap_arr.size();
+  
+    heapSort(heap_arr.data(), n);
+   
+    displayArray(heap_arr.data(), n);
+}
